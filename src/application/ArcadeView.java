ef568989bf8b4c4729dbbbf135feacf2a1c2d397
Melissa Buena <Mbuena@student.neumont.edu>
2017-02-22 18:44:00 -0700

    Added ArcadeView contents

--- src/application/ArcadeView.java
+++ src/application/ArcadeView.java
@@ -1,5 +1,17 @@
 package application;
 
-public class ArcadeView {
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class ArcadeView {
+	private static Stage primaryStage;
+	
+	public static void setScene(Scene scene){
+		primaryStage.setScene(scene);
+	
+	}
+	
+	public static void showStage(){
+		primaryStage.show();
+	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -14,8 +14,7 @@ public class Main extends Application {
 			BorderPane root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
