2bb232352b6b7a90e1d36610cc935ab591986074
Melissa Buena <Mbuena@student.neumont.edu>
2017-02-27 20:11:50 -0700

    Jumper Title Screen Buttons Work (Minus New Game)

--- jumper/application/Jumper.java
+++ jumper/application/Jumper.java
@@ -2,24 +2,29 @@ package application;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import models.Game;
 
 public class Jumper extends Game {
 	public static Scene titleScene;
+	public static Scene helpScene;
 
+	@FXML
 	@Override
 	public void exit() {
-		// TODO Auto-generated method stub
+		System.exit(0);
 		
 	}
 
+	@FXML
 	@Override
 	public void mainMenu() {
-		// TODO Auto-generated method stub
-		
+		showScene(MainMenu.getMenuScene(), "MCC Arcade");
 	}
 
 	@Override
@@ -27,11 +32,38 @@ public class Jumper extends Game {
 		showScene(titleScene, "Jumper");
 		
 	}
+	
+	@FXML
+	public void helpScreen(){
+		showScene(helpScene, "Jumper - Help");
+	}
+	
+	@FXML 
+	public void playJumper(ActionEvent e){
+		startGame(e);
+	}
+	
+	public void startGame(ActionEvent e){
+		JumperEngine.run();
+	}
 
+	@FXML
 	@Override
 	public void play() {
 		titleScene = createTitleScene();
+		helpScene = createHelpScene();
 		titleScreen();
+	}
+
+	private Scene createHelpScene() {
+		try{
+			Pane root = FXMLLoader.load(getClass().getResource("JumperHelpScene.fxml"));
+			Scene scene = new Scene(root);
+			return scene;
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	@Override
@@ -62,5 +94,15 @@ public class Jumper extends Game {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	@FXML
+	private Button menuButton;
+	@FXML 
+	private Button quitButton;
+	@FXML
+	private Button helpButton;
+	@FXML
+	private Button returnToTitleButton;
+	@FXML
+	private Button playButton;
 }
--- jumper/application/JumperEngine.java
+++ jumper/application/JumperEngine.java
@@ -5,4 +5,6 @@ public class JumperEngine extends Jumper{
 	public static void run(){
 		
 	}
+	
+	
 }
