1302f13bbc97e61f8be16fd5fd8a7711d9e04833
Melissa Buena <Mbuena@student.neumont.edu>
2017-02-28 18:13:20 -0700

    More Jumper Updates
    Most game logic works but needs fixing

--- jumper/application/JumperEngine.java
+++ jumper/application/JumperEngine.java
@@ -15,6 +15,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcBuilder;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 
@@ -34,7 +38,9 @@ public class JumperEngine extends Jumper{
 	private static Jumper j = new Jumper();
 	
 	private static int yJumpMotion;
+	private static AudioClip song;
 	private static AudioClip sound;
+	private static Rectangle wall;
 	
 	public static void run(){
 		startGame();
@@ -59,6 +65,7 @@ public class JumperEngine extends Jumper{
 		root = new Group();
 		gameScene = new Scene(root, WIDTH, HEIGHT, Color.TRANSPARENT);
 		
+		
 		Rectangle sky = new Rectangle(WIDTH, HEIGHT);
 		sky.setFill(new LinearGradient(0, 0, 0, 1, true, CycleMethod.NO_CYCLE, new Stop(0.0, Color.DODGERBLUE), new Stop(0.7, Color.LIGHTSKYBLUE), new Stop(1.0, Color.POWDERBLUE)));
 		root.getChildren().add(sky);
@@ -70,9 +77,16 @@ public class JumperEngine extends Jumper{
         root.getChildren().add(ground);
         
 		String musicFile = "jumper\\models\\jumperSong.mp3";
-		sound = new AudioClip(new File(musicFile).toURI().toString());		
-			sound.play();
+		song = new AudioClip(new File(musicFile).toURI().toString());		
+			song.play();
+			
+
         
+		wall = new Rectangle(100,75);
+		wall.setTranslateX(600);
+		wall.setTranslateY(325);
+		wall.setFill(Color.FIREBRICK);
+		root.getChildren().add(wall);
 	}
 	
 	public static void createJumper(){
@@ -81,16 +95,17 @@ public class JumperEngine extends Jumper{
 		llama.setImage(image);
 		llama.setFitWidth(140);
 		llama.setFitHeight(160);
-		llama.setLayoutX(150);
+		llama.setLayoutX(50);
 		llama.setLayoutY(255);
 		root.getChildren().add(llama);
 	}
 	
 	public static void jump(){
 		//llama.setTranslateY(yJumpMotion); 
-		yJumpMotion = 150;//adjust jumpheight
+		yJumpMotion = 350;//adjust jumpheight
 		if(isRunning){
 			llama.setTranslateY(llama.getTranslateY() - yJumpMotion);
+			
 		}
 	}
 	
@@ -103,6 +118,9 @@ public class JumperEngine extends Jumper{
 					jump();
 					System.out.println("Jump");
 					//donothing
+					String jumpSound = "jumper\\models\\jumpSound.mp3";
+					sound = new AudioClip(new File(jumpSound).toURI().toString());		
+						sound.play();
 				}
 			}
 			else if (event.getCode().equals(KeyCode.ESCAPE) && isRunning) {
@@ -119,36 +137,75 @@ public class JumperEngine extends Jumper{
 	public static void createJumperContent(){
 		createBackground();
 		createJumper();
+//		detectCollision();
 	}
+	
+	public static void detectCollision(){
+		if(wall.getBoundsInParent().intersects(llama.getBoundsInParent())){
+			System.out.println("collision");
+			String loseSound = "jumper\\models\\loseSound.mp3";
+			sound = new AudioClip(new File(loseSound).toURI().toString());		
+			sound.play();
+			j.gameOver();
+				
+		}
+	}
+	
+	
 	
 	public static void startGame(){
 		isRunning = true;
 		createJumperContent();
 		createKeyListener();
-		timer.schedule(new UpdateHandler(llama), 16, 16);
+		timer.schedule(new UpdateHandler(llama, wall), 32, 32);
 		timeline.play();
+	}
+	
+	public static void stopGame(){
+		song.stop();
+		timeline.stop();
+		timeline = new Timeline();
+		//timer.cancel();
+		//timer = new Timer();
+		isRunning = false;
+	}
+	
+	public static void restartGame() throws Exception{
+		run();
 	}
 	
 	private static class UpdateHandler extends TimerTask{
 
 		private ImageView llama;
+		private Rectangle wall;
 		
 
-		public UpdateHandler(ImageView llama){
+		public UpdateHandler(ImageView llama, Rectangle wall){
 			this.llama = llama;
+			this.wall = wall;
 		}
 		
 		@Override
 		public void run() {
-//			System.out.println(llama.getTranslateY()); 
+			detectCollision();
+			//			System.out.println(llama.getTranslateY()); 
 			if(llama.getTranslateY()<0){ //if llama is in the air
 				isJumping = true;
-				llama.setTranslateY(llama.getTranslateY() + 10);//bring back llama to ground
+				llama.setTranslateY(llama.getTranslateY() + 18);//bring back llama to ground
 			} 
 			else{
 				isJumping = false;
 			}
 			//move rectangles to the left put code in here.
+			if(isRunning){
+				wall.setTranslateX(wall.getTranslateX() - 16);	
+				if(wall.getTranslateX() <= 0){
+					wall.setTranslateX(700);
+				}
+			}
+			
+			
+			
 		}
 		
 	}
---
+++ jumper/models/JumperScore.java
@@ -0,0 +1,22 @@
+package models;
+
+public class JumperScore {
+	private static int score = 0;
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		JumperScore.score = score;
+	}
+	
+
+	public JumperScore() {
+
+	}
+
+	
+	
+
+}
