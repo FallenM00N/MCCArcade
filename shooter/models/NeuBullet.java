2934c30fcd7c0cf6208583f49782276cf2aeffd3
Caleb Carlile <ccarlile@student.neumont.edu>
2017-03-06 15:58:41 -0700

    Collision Detection. Still need score and more animation

--- shooter/models/NeuBullet.java
+++ shooter/models/NeuBullet.java
@@ -1,14 +1,17 @@
 package models;
 
-import javafx.scene.paint.Paint;
+import com.sun.org.glassfish.external.statistics.BoundaryStatistic;
+
+import interfaces.Collidable;
 import javafx.scene.shape.Rectangle;
 
-public class NeuBullet {
+public class NeuBullet implements Collidable{
 	private double x;
 	private double y;
 	private final double SPEED;
 	private final double width;
 	private final double height;
+	private Rectangle bounds;
 	
 	public double getWidth() {
 		return width;
@@ -32,6 +35,7 @@ public class NeuBullet {
 		this.SPEED = SPEED;
 		width = 5;
 		height = 3;
+		bounds = new Rectangle(x, y, width, height);
 	}
 	
 	public NeuBullet(double x, double y, double width, double height, double SPEED) {
@@ -40,10 +44,12 @@ public class NeuBullet {
 		this.SPEED = SPEED;
 		this.width = width;
 		this.height = height;
+		bounds = new Rectangle(x, y, width, height);
 	}
 	
 	public void move() {
 		x += SPEED;
+		bounds.setX(x);
 	}
 	
 	public void seek(NeuEnemy enemy) {
@@ -53,6 +59,7 @@ public class NeuBullet {
 		} else if(y < target) {
 			y += SPEED / 6;
 		}
+		bounds.setY(y);
 	}
 	
 	public void getTarget() {
@@ -69,6 +76,21 @@ public class NeuBullet {
 
 	public double getSPEED() {
 		return SPEED;
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		return bounds;
+	}
+
+	@Override
+	public boolean collides(Collidable object) {
+		boolean collides = false;
+		Rectangle otherBounds = object.getBounds();
+		if(otherBounds.getBoundsInParent().intersects(bounds.getBoundsInParent())) {
+			collides = true;
+		}
+		return collides;
 	}
 	
 }
--- shooter/models/SpaceShip.java
+++ shooter/models/SpaceShip.java
@@ -2,14 +2,18 @@ package models;
 
 import java.util.ArrayList;
 
+import interfaces.Collidable;
 import javafx.scene.image.Image;
+import javafx.scene.shape.Rectangle;
 
-public class SpaceShip {
+public class SpaceShip implements Collidable{
 	private double x;
 	private double y;
 	private double speed;
 	private ArrayList<NeuBullet> bullets = new ArrayList<>();
 	private Image image = new Image("neuImages/neuBlasterNoThrust.png");
+	Rectangle bounds;
+	private double score = 0;
 	
 	public Image getImage() {
 		return image;
@@ -23,11 +27,14 @@ public class SpaceShip {
 		this.x = x;
 		this.y = y;
 		this.speed = speed;
+		bounds = new Rectangle(x, y, getImage().getHeight(), getImage().getWidth());
 	}
 	
 	public void move (double dX, double dY) {
 		x += dX * speed;
 		y += dY * speed;
+		bounds.setX(x);
+		bounds.setY(y);
 	}
 
 	public double getX() {
@@ -36,6 +43,7 @@ public class SpaceShip {
 
 	public void setX(double x) {
 		this.x = x;
+		bounds.setX(x);
 	}
 
 	public double getY() {
@@ -44,6 +52,7 @@ public class SpaceShip {
 
 	public void setY(double y) {
 		this.y = y;
+		bounds.setY(y);
 	}
 
 	public double getSpeed() {
@@ -61,6 +70,20 @@ public class SpaceShip {
 	public void setBullets(ArrayList<NeuBullet> bullets) {
 		this.bullets = bullets;
 	}
+
+	@Override
+	public Rectangle getBounds() {
+		return bounds;
+	}
 	
+	@Override
+	public boolean collides(Collidable object) {
+		boolean collides = false;
+		Rectangle otherBounds = object.getBounds();
+		if(otherBounds.getBoundsInParent().intersects(bounds.getBoundsInParent())) {
+			collides = true;
+		}
+		return collides;
+	}
 	
 }
